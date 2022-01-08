818df834b4b6202296fc3223fc48cb844782541d
Vidit Varshney <viditvarshney222@gmail.com>
2022-01-08 13:50:48 +0530

    👌 IMPROVE: Calci

--- Day10/calci.py
+++ Day10/calci.py
@@ -1,5 +1,4 @@
-
-num1 = float(input("Enter 1st number: "))
+from logo import logo
 
 
 def add(n1, n2):
@@ -18,22 +17,37 @@ def divide(n1, n2):
     return n1 / n2
 
 
+symbols = ['+', '-', '/', '*']
 operations = {'+': add, '-': subtract,
               '*': multiply, '/': divide}
 
-for key in operations:
-    print(key)
 
-while True:
-    choice = input("Choose an operation: ")
-    num2 = float(input("Enter next number: "))
+def Calci():
+    print(logo)
+    num1 = float(input("Enter 1st number: "))
+    for key in operations:
+        print(key)
 
-    calculation_func = operations[choice]
-    result = calculation_func(num1, num2)
+    while True:
+        choice = input("Choose an operation: ")
+        if not choice in symbols:
+            print("WARNING! Invalid Operation symbol: ")
+            break
+        num2 = float(input("Enter next number: "))
 
-    print(f"{num1} {choice} {num2} = {result}")
-    if input(f"Type 'y to continue with 9.0 or 'n' to exit: ").casefold() == 'y':
-        num1 = result
-    else:
-        print(f"Your final result is: {result}")
-        break
+        calculation_func = operations[choice]
+        result = calculation_func(num1, num2)
+        print(f"{num1} {choice} {num2} = {result}")
+        clear = input(
+            f"Type 'y to continue with {result} or 'new' to start a new calculation  'n' to exit: ")
+
+        if clear.casefold() == 'y':
+            num1 = result
+        elif clear.casefold() == 'new':
+            Calci()
+        else:
+            print(f"Your final result is: {result}")
+            break
+
+
+Calci()
