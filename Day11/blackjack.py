d2faceb7da687f3ff09df7bd77f4152436e4293f
Vidit Varshney <viditvarshney222@gmail.com>
2022-01-10 03:40:41 +0530

    👌 IMPROVE: blackJack

--- Day11/blackjack.py
+++ Day11/blackjack.py
@@ -1,39 +1,88 @@
 import random
+from typing import Generator
 
 # Cards Numbers
 # Ace is 11, J,K,Q (each value is 10)
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+
+def Score(cards):
+    """Returns the total score after checking blackjack or sum greater than 21"""
+
+    # check for a blackjack (a hand with only 2 cards: ace + 10)
+    #  and return 0 instead of the actual score. 0 will represent a blackjack in the game.
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    #  check for 11  in the list,
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+        # print("Your score exceeds 21 so '11' is replaced by '1'.")
+    return sum(cards)
+
+
+# generates a new random card from the list
+
+
+def rand_generator():
+    """Draws a random card"""
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+
+def compare(user_score, dealer_score):
+    if user_score == dealer_score:
+        return "It's a draw"
+    elif user_Score == 0:
+        return "You Won with BLACKJACK"
+    elif dealer_Score == 0:
+        return "You lose! Dealer has the BLACKJACK"
+    elif user_Score > 21 or (user_Score > 21 and dealer_Score > 21):
+        return "You Lose! You went over 21"
+    elif dealer_Score > 21:
+        return "You Won, Dealer went over 21"
+    elif user_Score > dealer_Score:
+        return "You Win..."
+    else:
+        return "You lose..."
+# random two caards for user and dealer
+
+
 player = []
 dealer = []
 
-rand_n1 = player.append(random.choice(cards))
-rand_n2 = player.append(random.choice(cards))
+game_over = False
 
-dealer_rand1 = dealer.append(random.choice(cards))
+for _ in range(2):
+    player.append(rand_generator())
+    dealer.append(rand_generator())
 
+while not game_over:
 
-def isAce(random_cards_list):
-    if 11 in random_cards_list:
-        return True
+    user_Score = Score(player)
+    dealer_Score = Score(dealer)
+    game_over = False
+
+    print(f"Your cards: {player}, and score: {user_Score}")
+    print(f"dealer cards: {dealer[0]}")
+
+    if user_Score == 0 or dealer_Score == 0 or sum(player) > 21:
+        game_over = True
     else:
-        return False
+        if input("You want a HIT? 'y' or 'n' for stand: ").casefold() == 'y':
+            player.append(rand_generator())
+            # print(player)
 
+        else:
 
-def is10(random_cards_list):
-    if 10 in random_cards_list:
-        return True
-    else:
-        return False
+            game_over = True
 
-
-def isBlackJack(player_list):
-    if isAce(player_list) == True and is10(player_list) == True:
-        return True
-    else:
-        return False
-# check if player got the blackjack? If yes : player wins
-
-
-if isBlackJack(player):
-    print("You wins as u have blackjack.")
+    while dealer_Score > 0 and dealer_Score < 17:
+        dealer.append(rand_generator())
+        dealer_Score = Score(dealer)
+print(f"   Your final hand: {player}, final score: {user_Score}")
+print(
+    f"   Computer's final hand: {dealer}, final score: {dealer_Score}")
+print(compare(user_Score, dealer_Score))
