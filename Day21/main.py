c5a63a2fc4b2141db5d74aa38d0e8dcca049499d
Vidit Varshney <viditvarshney222@gmail.com>
2022-01-29 23:00:29 +0530

    📦 NEW:  Added collison with Food Functionality

---
+++ Day21/main.py
@@ -0,0 +1,36 @@
+import time
+import turtle as t
+from snake import Snake
+from food import Food
+
+
+my_screen = t.Screen()
+my_screen.bgcolor("blue")
+my_screen.setup(height=600, width=600)
+
+my_screen.tracer(0)
+# # Create Snake body
+
+snaky = Snake()
+food = Food()
+my_screen.listen()
+my_screen.onkey(key="Left", fun=snaky.move_left)
+my_screen.onkey(key="Up", fun=snaky.move_up)
+my_screen.onkey(key="Down", fun=snaky.move_down)
+my_screen.onkey(key="Right", fun=snaky.move_right)
+# Snake Moving
+
+is_snake_moving = True
+while is_snake_moving:
+    my_screen.update()
+    time.sleep(0.1)
+    snaky.move()
+
+    # Detec Collison of Snake With Food
+
+    if snaky.head.distance(food) < 15:
+        print("collison")
+        food.reset_food()
+
+
+my_screen.exitonclick()
